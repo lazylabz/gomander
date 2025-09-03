0824e1231476daa57c5f6218fd22265587f6cd44
Noel Lopez <nlopez.publi@gmail.com>
2025-09-03 23:35:50 +0200

    feat: add sitemap integration and SEO files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    site: 'https://lazylabz.github.io',
+    base: '/gomander',
+    trailingSlash: 'ignore',
+    integrations: [
+        sitemap({
+            changefreq: 'weekly',
+            priority: 0.7,
+            lastmod: new Date(),
+            customPages: ['https://lazylabz.github.io/gomander/'],
+        })
+    ],
+    vite: {
+        plugins: [tailwindcss()]
+    }
 });
